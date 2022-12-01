98f1011b493c26a1d0ef18c48653bc9c42568c5d
Michał Krzempek <116187369+miskrz@users.noreply.github.com>
2022-12-01 20:15:49 +0100

    Stworzenie pliku

---
+++ conflictfile3.py
@@ -0,0 +1,18 @@
+licznik = 0
+while True:
+    print licznik,
+    licznik += 1
+    if licznik >= 5:
+        break
+licznik = 0
+while True:
+    print licznik,
+    licznik += 1
+    if licznik >= 5:
+        break
+licznik = 0
+while True:
+    print licznik,
+    licznik += 1
+    if licznik >= 5:
+        break
