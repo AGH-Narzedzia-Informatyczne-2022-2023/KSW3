bb9e61da990be88bfce52fa0625e1cde60b595d5
Piotr Waluszek <115178409+waluszekp@users.noreply.github.com>
2022-12-01 19:39:48 +0100

    Stworzenie pliku do konfliktu

---
+++ conflicfile.py
@@ -0,0 +1,13 @@
+licznik = 0
+while True:
+    print licznik,
+    licznik += 1
+    if licznik >= 5:
+        break
+
+
+for x in xrange(10):
+    # Sprawdz, czy x jest parzyste
+    if x % 2 == 0:
+        continue
+    print x,
